066a07c6e28a08d6fc868cbeb0f9746f9add8b3e
ZULVER2025 <duvanfelipesanchezvillamil@gmail.com>
2026-08-24 20:47:35 -0500

    Actualizacion

--- database/migrations/clientes.php
+++ database/migrations/clientes.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('clientes', function (Blueprint $table) {
+        Schema::create('clientes', function (Blueprint $table) {
 
          $table->id();
          $table->string("documento",20)->unique();
