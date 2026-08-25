1b3f4ae33db1f02a75591dcd1d2dab9ed443e5b7
Santiago <eduarsantiago610@gmail.com>
2026-08-24 20:27:16 -0500

    feat:roles

---
+++ database/migrations/2026_08_25_012613_create_roles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->unique(); // Columna para el nombre del rol
+            $table->string('descripcion')->nullable(); // Columna opcional para la descripción
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
