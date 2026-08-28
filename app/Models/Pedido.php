67c6259fffde221d4c43929acff3de73d94909a0
Anderson Sanchez <anderson.asm201@gmail.com>
2026-08-27 20:37:00 -0500

    se crea el modelo pedido y se configura sus relaciones

---
+++ app/Models/Pedido.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;    
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Pedido extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pedidos';
+
+    protected $fillable = [
+        'cliente_id',
+        'usuario_id',
+        'metodo_pago',
+        'estado',
+        'total',
+    ];
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class, 'cliente_id');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'usuario_id');
+    }   
+    public function detallePedidos()
+    {
+        return $this->hasMany(DetallePedido::class);
+    }
+     public function pagos ()
+    {
+        return $this->hasMany(Pago::class);
+    }
+
+    public function producciones  ()
+    {
+        return $this->hasMany(Producciones ::class);
+    }
+
+    public function historialPedidos ()
+    {
+        return $this->hasMany(HistorialPedido::class);
+    }
+
+}
