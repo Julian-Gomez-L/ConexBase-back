bc5cd3bd04defa69ee134917f94afee43d657e51
artista <juliangomez.sena.class@gmail.com>
2026-08-27 19:05:48 -0500

    create model produccion

---
+++ app/Models/produccion.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class produccion extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produccion';
+
+    protected $fillable = [
+        'id',
+        'fecha_inicio',
+        'fecha_fin',
+        'estado',
+        'observaciones',
+        'id_pedido',
+        'id_producto',
+        'id_usuario',
+        'id_trabajos_tapiceria',
+    ];
+
+        public function pedido()
+    {
+        return $this->belongsTo(Pedido::class, 'id_pedido');
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, 'id_producto');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'id_usuario');
+    }
+
+        public function trabajos_tapiceria()
+    {
+        return $this->hasMany(Trabajos_tapiceria::class, 'id_trabajos_tapiceria');
+    }
+}
